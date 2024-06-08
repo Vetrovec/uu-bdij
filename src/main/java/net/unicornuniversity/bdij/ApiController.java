4b84a24fc45660483d82c28ac077a4d75d4300e6
Simon <s.kristin@cleo.one>
2024-06-08 15:31:55 +0200

    controller refactored

--- src/main/java/net/unicornuniversity/bdij/ApiController.java
+++ src/main/java/net/unicornuniversity/bdij/ApiController.java
@@ -2,6 +2,7 @@ package net.unicornuniversity.bdij;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
+import net.unicornuniversity.bdij.dtos.UpdateIcoDto;
 import net.unicornuniversity.bdij.entities.IcoDataEntity;
 import net.unicornuniversity.bdij.exceptions.InvalidIcoException;
 import net.unicornuniversity.bdij.exceptions.NotFoundException;
@@ -16,8 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,14 +33,109 @@ public class ApiController {
 
     @GetMapping("/{ico}")
     public IcoData getIcoData(@PathVariable String ico) throws InvalidIcoException, NotFoundException, UnknownErrorException {
+        IcoDataEntity entity = getIcoDataEntityOrThrow(ico);
 
+        return IcoData.from(entity);
+    }
+
+    @PostMapping()
+    public IcoData saveIcoData(@RequestBody IcoData icoData) throws InvalidIcoException, UnknownErrorException {
+
+        Optional<IcoDataEntity> optionalEntity = icoDataRepository.findById(icoData.getIco());
+        if (optionalEntity.isPresent()) {
+            throw new OperationForbiddenException("ICO: " + icoData.getIco() + " already exists");
+        }
+
+        String ico = icoData.getIco();
+        if (!ico.matches("\\d{8}")) {
+            throw new InvalidIcoException("ICO must be an 8-digit number: " + ico);
+        }
+
+        // ideally we would validate other fields here as well
+
+        try {
+            IcoDataEntity entity = IcoDataEntity.from(icoData);
+            icoDataRepository.save(entity);
+            return icoData;
+        } catch (Exception e) {
+            throw new UnknownErrorException("An error occurred while saving data for ICO: " + ico, e);
+        }
+    }
+
+    @DeleteMapping("/{ico}")
+    public void deleteIcoData(@PathVariable String ico) throws InvalidIcoException, NotFoundException, UnknownErrorException, OperationForbiddenException {
+
+        IcoDataEntity entity = getIcoDataEntityOrThrow(ico);
+
+        if (entity.getDeleted()) {
+            throw new OperationForbiddenException("Operation forbidden for ICO: " + ico);
+        }
+
+        entity.setDeleted(true);
+        icoDataRepository.save(entity);
+    }
+
+    @PatchMapping("/{ico}")
+    public IcoData patchIcoData(@PathVariable String ico, @RequestBody UpdateIcoDto body) throws InvalidIcoException, NotFoundException, UnknownErrorException {
+        try {
+            IcoDataEntity icoDataEntity = getIcoDataEntityOrThrow(ico);
+
+            String obchodniJmeno = body.getObchodniJmeno();
+            if (obchodniJmeno != null) {
+                icoDataEntity.setObchodniJmeno(obchodniJmeno);
+            }
+
+            String financniUrad = body.getFinancniUrad();
+            if (financniUrad != null) {
+                icoDataEntity.setFinancniUrad(financniUrad);
+            }
+
+            String radekAdresy1 = body.getRadekAdresy1();
+            if (radekAdresy1 != null) {
+                icoDataEntity.setRadekAdresy1(radekAdresy1);
+            }
+
+            String radekAdresy2 = body.getRadekAdresy2();
+            if (radekAdresy2 != null) {
+                icoDataEntity.setRadekAdresy2(radekAdresy2);
+            }
+
+            String radekAdresy3 = body.getRadekAdresy3();
+            if (radekAdresy3 != null) {
+                icoDataEntity.setRadekAdresy3(radekAdresy3);
+            }
+
+            icoDataRepository.save(icoDataEntity);
+
+            return IcoData.from(icoDataEntity);
+        } catch (Exception e) {
+            throw new UnknownErrorException("An error occurred while updating data for ICO: " + ico, e);
+        }
+    }
+
+    @GetMapping("/search")
+    public List<IcoDataEntity> searchByName(@RequestParam String name) throws UnknownErrorException {
+        try {
+            Pageable limitPageable = PageRequest.of(0, 20);
+
+            return icoDataRepository.searchByFullName(name, limitPageable);
+        } catch (Exception e) {
+            throw new UnknownErrorException("An error occurred while searching for:" + name, e);
+        }
+    }
+
+    private IcoDataEntity getIcoDataEntityOrThrow(String ico) throws InvalidIcoException, NotFoundException, UnknownErrorException {
         if (!ico.matches("\\d{8}")) {
             throw new InvalidIcoException("ICO must be an 8-digit number: " + ico);
         }
 
         Optional<IcoDataEntity> optionalEntity = icoDataRepository.findById(ico);
         if (optionalEntity.isPresent()) {
-            return IcoData.from(optionalEntity.get());
+            if (optionalEntity.get().getDeleted()) {
+                throw new NotFoundException("Data not found for ICO: " + ico);
+            }
+
+            return optionalEntity.get();
         }
 
         RestTemplate restTemplate = new RestTemplate();
@@ -52,7 +148,7 @@ public class ApiController {
             IcoDataEntity entity = IcoDataEntity.from(aresResult);
             icoDataRepository.save(entity);
 
-            return aresResult;
+            return entity;
         } catch (HttpClientErrorException e) {
             if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
                 throw new NotFoundException("Data not found for ICO: " + ico);
@@ -60,80 +156,5 @@ public class ApiController {
                 throw new UnknownErrorException("An error occurred while retrieving data for ICO: " + ico, e);
             }
         }
-    }
-
-    @PatchMapping("/{ico}")
-    public IcoDataEntity patchIcoData(@PathVariable String ico, HttpServletRequest request) throws IOException    {
-        IcoDataEntity entity = getEntityOrThrow(ico);
-
-        BufferedReader reader = request.getReader();
-        IcoDataEntity updatedEntity = objectMapper.readerForUpdating(entity).readValue(reader);
-        icoDataRepository.save(updatedEntity);
-        return updatedEntity;
-    }
-
-    @PostMapping()
-    public IcoData saveIcoData(@RequestBody IcoData icoData) {
-        IcoDataEntity entity = IcoDataEntity.from(icoData);
-
-        icoDataRepository.save(entity);
-        return icoData;
-    }
-
-    private IcoDataEntity getEntityOrThrow(String ico) {
-        Optional<IcoDataEntity> optionalEntity = icoDataRepository.findById(ico);
-        if (optionalEntity.isEmpty()) {
-            throw new NotFoundException("Entity not found");
-        }
-
-        IcoDataEntity entity = optionalEntity.get();
-        if (entity.getDeleted()) {
-            throw new OperationForbiddenException("Operation forbidden on deleted entity");
-        }
-
-        return entity;
-    }
-
-    @DeleteMapping("/{ico}")
-    public void deleteIcoData(@PathVariable String ico)
-    {
-        Optional<IcoDataEntity> optionalEntity = icoDataRepository.findById(ico);
-        if (optionalEntity.isPresent())
-        {
-            IcoDataEntity entity = optionalEntity.get();
-
-            if (entity.getDeleted())
-            {
-                // TODO: Operation forbidden exception with handling
-                throw new RuntimeException("Operation forbidden");
-            }
-
-            entity.setDeleted(true);
-            icoDataRepository.save(entity);
-            return;
-        }
-
-        // TODO: Remove repeating code
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico;
-        IcoData data = restTemplate.getForObject(url, IcoData.class);
-
-        if (data == null)
-        {
-            // TODO: Not found exception with handling
-            throw new RuntimeException("Not found");
-        }
-
-        IcoDataEntity entity = IcoDataEntity.from(data);
-        entity.setDeleted(true);
-        icoDataRepository.save(entity);
-    }
-
-    @GetMapping("/search")
-    public List<IcoDataEntity> searchByName(@RequestParam String name)
-    {
-        Pageable limitPageable = PageRequest.of(0, 20);
-        List<IcoDataEntity> entities = icoDataRepository.searchByFullName(name, limitPageable);
-        return entities;
     }
 }
