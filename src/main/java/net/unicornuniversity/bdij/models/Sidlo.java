1b58fcf95be1bea5d4d01e5d5874e38b2a81237a
Simon <s.kristin@cleo.one>
2024-06-08 11:37:51 +0200

    sidlo get/set

--- src/main/java/net/unicornuniversity/bdij/models/Sidlo.java
+++ src/main/java/net/unicornuniversity/bdij/models/Sidlo.java
@@ -3,4 +3,10 @@ package net.unicornuniversity.bdij.models;
 public class Sidlo {
     private String kodStatu;
     private String nazevStatu;
+
+    public String getKodStatu() { return kodStatu; }
+    public void setKodStatu(String kodStatu) { this.kodStatu = kodStatu; }
+
+    public String getNazevStatu() { return nazevStatu; }
+    public void setNazevStatu(String nazevStatu) { this.nazevStatu = nazevStatu; }
 }
