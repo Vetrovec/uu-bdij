78c94bf20939683393c3cc84ffad8391aeb9c03f
Stepan Vetrovec <s.vetrovec@cleo.one>
2024-06-08 10:09:01 +0200

    Spring

---
+++ src/main/java/net/unicornuniversity/bdij/BdijApplication.java
@@ -0,0 +1,12 @@
+package net.unicornuniversity.bdij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdijApplication.class, args);
+	}
+}
---
+++ src/test/java/net/unicornuniversity/bdij/BdijApplicationTests.java
@@ -0,0 +1,13 @@
+package net.unicornuniversity.bdij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
