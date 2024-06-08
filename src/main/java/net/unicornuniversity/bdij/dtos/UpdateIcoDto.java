85dd76d5dfb71a578e4f62e0d773485afe692f88
Stepan Vetrovec <s.vetrovec@cleo.one>
2024-06-08 15:21:18 +0200

    Add update dto

---
+++ src/main/java/net/unicornuniversity/bdij/dtos/UpdateIcoDto.java
@@ -0,0 +1,49 @@
+package net.unicornuniversity.bdij.dtos;
+
+public class UpdateIcoDto {
+    private String obchodniJmeno;
+    private String financniUrad;
+    private String radekAdresy1;
+    private String radekAdresy2;
+    private String radekAdresy3;
+
+    public String getObchodniJmeno() {
+        return obchodniJmeno;
+    }
+
+    public void setObchodniJmeno(String obchodniJmeno) {
+        this.obchodniJmeno = obchodniJmeno;
+    }
+
+    public String getFinancniUrad() {
+        return financniUrad;
+    }
+
+    public void setFinancniUrad(String financniUrad) {
+        this.financniUrad = financniUrad;
+    }
+
+    public String getRadekAdresy1() {
+        return radekAdresy1;
+    }
+
+    public void setRadekAdresy1(String radekAdresy1) {
+        this.radekAdresy1 = radekAdresy1;
+    }
+
+    public String getRadekAdresy2() {
+        return radekAdresy2;
+    }
+
+    public void setRadekAdresy2(String radekAdresy2) {
+        this.radekAdresy2 = radekAdresy2;
+    }
+
+    public String getRadekAdresy3() {
+        return radekAdresy3;
+    }
+
+    public void setRadekAdresy3(String radekAdresy3) {
+        this.radekAdresy3 = radekAdresy3;
+    }
+}
