824bcc6d751298b0646a42d7bccb560c9f05c928
Stepan Vetrovec <s.vetrovec@cleo.one>
2024-06-08 14:26:55 +0200

    Add search method to repository

--- src/main/java/net/unicornuniversity/bdij/repositories/IcoDataRepository.java
+++ src/main/java/net/unicornuniversity/bdij/repositories/IcoDataRepository.java
@@ -1,9 +1,16 @@
 package net.unicornuniversity.bdij.repositories;
 
 import net.unicornuniversity.bdij.entities.IcoDataEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IcoDataRepository extends JpaRepository<IcoDataEntity, String> {
+    @Query("SELECT i FROM IcoDataEntity i WHERE LOWER(i.obchodniJmeno) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<IcoDataEntity> searchByFullName(@Param("name") String name, Pageable pageable);
 }
