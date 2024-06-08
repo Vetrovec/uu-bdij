9f50ae9b210cea223497ccfda4c0dc26deb60caa
Simon <s.kristin@cleo.one>
2024-06-08 10:50:29 +0200

    add basic get endpoint for ares

--- src/main/java/net/unicornuniversity/bdij/BdijApplication.java
+++ src/main/java/net/unicornuniversity/bdij/BdijApplication.java
@@ -3,8 +3,10 @@ package net.unicornuniversity.bdij;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
@@ -17,5 +19,14 @@ public class BdijApplication {
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/api/{ico}")
+	public String getIcoData(@PathVariable String ico) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "https://ares.gov.cz/ekonomicke-subjekty-v-be/rest/ekonomicke-subjekty/" + ico;
+
+		String result = restTemplate.getForObject(url, String.class);
+		return result;
 	}
 }
